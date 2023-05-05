811cafc3c658c7e972850aa2f22715a2daa2f2f6
MikayelSughyan <103155834+MikayelSughyan@users.noreply.github.com>
2023-05-04 21:25:55 -0700

    refactored main

--- src/main/App.java
+++ src/main/App.java
@@ -1,3 +1,4 @@
+package main;
 // gui libraries
 import java.awt.BorderLayout;
 import java.awt.Color;
--- src/main/GPTHandler.java
+++ src/main/GPTHandler.java
@@ -1,3 +1,4 @@
+package main;
 import java.io.*;
 import java.net.URI;
 //import java.net.URISyntaxException;

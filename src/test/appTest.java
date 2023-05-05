381d8992c9d799ba32077995aed18bd4b703aefb
MikayelSughyan <103155834+MikayelSughyan@users.noreply.github.com>
2023-05-04 21:18:05 -0700

    Add first test

---
+++ src/test/appTest.java
@@ -0,0 +1,18 @@
+package test;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+public class appTest {
+    @Test
+    void testNothing() {
+        
+    }
+}

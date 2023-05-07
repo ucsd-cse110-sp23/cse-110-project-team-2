9e76a796d3d4486386ca40fcf938733470320d8c
amaroguy <amaroguy@gmail.com>
2023-05-07 01:14:27 -0700

    Added Mock Whisper class

---
+++ src/main/MockWhisper.java
@@ -0,0 +1,17 @@
+import java.io.File;
+
+/**
+ * Mock Whisper API, do not waste API credits.
+ * "Hard Code" a response from transcribeAudio
+ */
+public class MockWhisper {
+    private String transcription;
+    
+    public MockWhisper(String transcription){
+        this.transcription = transcription;
+    }
+
+    public String transcribeAudio(File Audio){
+        return transcription;
+    }
+}

709a970e087fa8376b11a2413ecb94956c3812a6
amaroguy <amaroguy@gmail.com>
2023-05-11 20:46:32 -0700

    Made HistoryManager class, adapted methods from GUI code into it

---
+++ src/main/HistoryManager.java
@@ -0,0 +1,76 @@
+import java.util.ArrayList;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.util.Scanner;
+import java.io.File;
+
+public class HistoryManager {
+    private ArrayList<QNA> historyList;
+    private File historyFile;
+
+    public HistoryManager(String historyFilePath){
+        historyList = new ArrayList<QNA>();
+        historyFile = new File("history.txt");
+        historyList = new ArrayList<QNA>();
+        
+        //Load a history file into a variable, build the readers/writers.
+        try{
+            if(!historyFile.exists()){
+                historyFile.createNewFile();
+            }
+            readFileIntoArrayList();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        
+    }
+    
+    private void readFileIntoArrayList(){
+        //Read the file into the ArrayList
+        String tempQuestion;
+        String tempAnswer;
+
+        try{
+            FileReader fr = new FileReader(historyFile);
+            BufferedReader br = new BufferedReader(fr);
+            Scanner sr = new Scanner(br);
+            sr.useDelimiter(",,,");
+            while (sr.hasNext()) {
+                tempQuestion = sr.next();
+                tempAnswer = sr.next();
+                historyList.add(new QNA(tempQuestion,tempAnswer));
+            }
+            sr.close();
+            br.close();
+            fr.close();
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    
+    public ArrayList<QNA> getHistoryList(){
+        return historyList;
+    }
+
+    public void addToHistory(QNA question){
+        try{
+            FileWriter history = new FileWriter(historyFile, true);
+            history.write(question.getQuestion() + ",,," + question.getAnswer() + ",,,");
+            history.close();
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+        return;
+    }
+
+    public void editHistory(QNA question){
+        return;
+    }
+
+    public void deleteFromHistory(QNA question){
+        return;
+    }
+}
--- src/main/Question.java
+++ src/main/Question.java
@@ -1,16 +1,11 @@
-public class QNA {
+public class Question {
     private String question;
     private String answer;
 
-    public QNA(String question, String answer){
+    public Question(String question, String answer){
         this.question = question;
         this.answer = answer;
     }
-
-    public boolean equals(QNA otherQuestion){
-        return this.question.equals(otherQuestion.getQuestion()) && this.answer.equals(otherQuestion.getAnswer());
-    }
-
 
     public void setAnswer(String answer){
         this.answer = answer;

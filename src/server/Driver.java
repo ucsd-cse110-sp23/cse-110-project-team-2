0bc51af6bdec531836d51f216dabfd8c887adfa8
amaroguy <amaroguy@gmail.com>
2023-06-05 22:59:06 -0700

    finished mongohandler I think

--- src/server/Driver.java
+++ src/server/Driver.java
@@ -8,7 +8,8 @@ public class Driver {
         // mongo.addPrompt("foo","question","hi there", "abc");
         // mongo.updateUserEmailInfo("foo", "google", "1", "example@gmail.com", "hunter2");
         // mongo.updateUserEmailInfo("foo", "bing", "1", "example@gmail.com", "hunter2");
-        mongo.modifyPromptBody("foo", "647dd1d0409aa55cfb3df283", "new body");
-        
+        mongo.getAllUserPrompts("baz");
+        mongo.deletePrompt("foo", "647dd1d0409aa55cfb3df284");  
+        mongo.addUser("aaa","aaa");      
     }
 }
--- src/server/MongoHandler.java
+++ src/server/MongoHandler.java
@@ -9,6 +9,7 @@ import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import static java.util.Arrays.asList;
+import java.util.ArrayList;
 
 import java.nio.file.attribute.UserPrincipalNotFoundException;
 
@@ -23,7 +24,7 @@ import com.mongodb.client.result.UpdateResult;
 import org.bson.Document;
 import org.bson.conversions.Bson;
 import org.bson.json.JsonWriterSettings;
-
+import com.mongodb.client.result.InsertOneResult;
 
 import static com.mongodb.client.model.Filters.and;
 import static com.mongodb.client.model.Filters.eq;
@@ -45,7 +46,15 @@ public class MongoHandler {
         promptCollection = db.getCollection("Prompts"); 
     }
 
-    public int addUser(String username, String password){
+
+    /**
+     * Creates a user in the db. 
+     * 
+     * @param username - the user's username
+     * @param password - the user's password
+     * @return true if the user was successfully added, false if something went wrong
+     */
+    public boolean addUser(String username, String password){
 
         Document user = new Document();
         Document emailInfo = new Document();
@@ -61,76 +70,149 @@ public class MongoHandler {
             .append("prompts", asList())
             .append("emailInfo", emailInfo);
 
-        userInfoCollection.insertOne(user);
+        InsertOneResult ir = userInfoCollection.insertOne(user);
 
-        //Http Status OK
-        return 200;
+        return ir.wasAcknowledged();
     }
 
-    //TODO method header is NOT final.
-    public String addPrompt(String username, String promptType, String prompt, String promptBody){
+    /**
+     * Gets all prompts associated with a user.
+     * 
+     * @param username - the username of the user whose documents you're getting
+     * @return Returns an ArrayList of MongoDB Documents, null if the user does not exist. 
+     */
+    public ArrayList<Document> getAllUserPrompts(String username){
+        
+        Bson filter = eq("username",username);
+        ArrayList<Document> query = (ArrayList<Document>) userInfoCollection.find(filter).first().get("prompts");
+        
+        
 
+        System.out.println(query.size());
+        
+        return query;
+    }
+    
+    /**
+     * Adds a prompt to a user's profile.
+     * 
+     * @param username - the user's username
+     * @param promptType - the type of the prompt
+     * @param prompt - the command "Create email to X"
+     * @param promptBody - "the response of the prompt"
+     * @return true if the prompt was successfully added and false if something went wrong
+     */
+    public boolean addPrompt(String username, String promptType, String prompt, String promptBody){
+        
         //make the new prompt object/document
         Document newPrompt = new Document();
         newPrompt.append("_id", new ObjectId())
-                    .append("promptType", promptType)
-                    .append("prompt", prompt)
-                    .append("promptBody", promptBody);
-
+        .append("promptType", promptType)
+        .append("prompt", prompt)
+        .append("promptBody", promptBody);
+        
         //filter db query to only select the user we want
         Bson filter = eq("username", username);
-
+        
         //set the operation to push it to the user's prompts field.
         Bson updateOperation = push("prompts", newPrompt);
-
+        
         //apply the update to the db
         UpdateResult updateResult = userInfoCollection.updateOne(filter,updateOperation);
-
-
-        return "foo";
+        
+        return updateResult.getModifiedCount() == 1;
     }
+    
 
-    public String updateUserEmailInfo(String username, String smtpHost, String smtpPort, String email, String emailPassword){
+    /**
+     * Updates a user's email info
+     * 
+     * @param username
+     * @param smtpHost
+     * @param smtpPort
+     * @param email
+     * @param emailPassword
+     * @return true if the info was successfully updated, and false if something went wrong
+     */
+    public boolean updateUserEmailInfo(String username, String smtpHost, String smtpPort, String email, String emailPassword){
         
         //Instead of changing one field I'm just gonna replace the
         //email document in the user entirely
         Document emailInfo = new Document();
         emailInfo.append("smtpPort", smtpPort)
-            .append("smtpHost",smtpHost)
-            .append("email",email)
-            .append("emailPassword",emailPassword);
-
+        .append("smtpHost",smtpHost)
+        .append("email",email)
+        .append("emailPassword",emailPassword);
+        
         Bson filter = eq("username", username);
         Bson updateOperation = set("emailInfo", emailInfo);
-
+        
         UpdateResult updateResult = userInfoCollection.updateOne(filter,updateOperation);
-
-        return "foo";
+        
+        return updateResult.getModifiedCount() == 1;
     }
 
-    //prompts should have a unique identifier, dunno what it'll be but they should 
-    public String deletePrompt(String username, String promptID){
+    /**
+     * Gets the email info of a user. 
+     * 
+     * @param username - the username of the user whose info we're trying to get
+     * @return the user's email info
+     */
+    public Document getUserEmailInfo(String username){
 
+        Bson filter = eq("username",username);
+        Document emailInfo = (Document) userInfoCollection.find(filter).first().get("emailInfo");     
+        
+        return emailInfo;
+    }
+
+    /**
+     * Gets a user object
+     * @param username - username of the user we're querying
+     * @return the user's document
+     */
+    public Document getUser(String username){
+        Bson filter = eq("username", username);
+        return userInfoCollection.find(filter).first();
+    }
+
+
+    /**
+     * Delete a user's prompt
+     * @param username - username of user
+     * @param promptID - the ID of the prompt you want to delete.
+     * @return true if something was deleted, false if nothing was deleted.
+     */ 
+    public boolean deletePrompt(String username, String promptID){
+        
         //what user does this prompt belong to
         Bson userFilter = eq("username", username);
-
+        
         //remove the document in the prompts array that has the id of promptID
         Bson deleteOperation = pull("prompts", new Document().append("_id", new ObjectId(promptID)));
-
+        
         UpdateResult updateResult = userInfoCollection.updateOne(userFilter,deleteOperation);
-        return "foo";
+        
+        return updateResult.getModifiedCount() == 1;
     }
-
-    public String modifyPromptBody(String username, String promptID, String newBody){
+    
+    /**
+     * Modifies the bnody of a prompt
+     * @param username
+     * @param promptID
+     * @param newBody - the new body to replace the old body
+     * @return true if the modification was successful and false if otherwise
+     */
+    public boolean modifyPromptBody(String username, String promptID, String newBody){
         Bson a = eq("username",username);
         Bson b = eq("prompts._id", new ObjectId(promptID));
         Bson q = combine(a,b);
-
+        
         Bson setOperation = set("prompts.$.promptBody", newBody);
 
         UpdateResult ur = userInfoCollection.updateOne(q, setOperation);
-
-        return "foo";
+        
+        return ur.getModifiedCount() == 1;
     }
 
     public static void main(String[] args){

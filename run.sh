7b72a6c22b0e82aa72458a798b75d4cc5d552095
MikayelSughyan <103155834+MikayelSughyan@users.noreply.github.com>
2023-06-08 01:49:42 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,10 +4,12 @@
 #find bin/ -name "*.class" -type f -delete
 
 # compile
+javac -cp lib/json-20230227.jar:. -d bin src/server/*.java
 javac -cp lib/json-20230227.jar:. -d bin src/main/*.java
 
 # run
 cd bin
+java -cp ../lib/json-20230227.jar:. SayItServer
 java -cp ../lib/json-20230227.jar:. App
 cd ..
 
